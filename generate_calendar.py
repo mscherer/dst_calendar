09efb4961d430167278a0b80d01ff17e44cb0af6
Michael Scherer <misc@redhat.com>
2022-03-14 13:33:09 +0100

    Turn the file in a library

--- generate_calendar.py
+++ generate_calendar.py
@@ -4,61 +4,65 @@ import datetime
 from ics import Calendar, Event
 import hashlib 
 
-NB_TZ = len(sys.argv)
+
 NB_CHANGE = 2
-
-if NB_TZ < 2:
-    print("error")
-    sys.exit(1)
-tzs = []
-for i in sys.argv[1:]:
-    tzs.append(pytz.timezone(i))
-
-def get_tz_transition_time(tz, year):
-    return [x for x in tz._utc_transition_times if x.year == year]
-
 START_YEAR = datetime.datetime.today().year - 2
 END_YEAR = datetime.datetime.today().year + 5
 OUTPUT = 'timezone.ics'
 
-cal = Calendar()
+def get_tzs_args():
+    if len(sys.argv) < 2:
+        print("error")
+        sys.exit(1)
+    tzs = []
+    for i in sys.argv[1:]:
+        tzs.append(pytz.timezone(i))
+    return tzs
 
-for y in range(START_YEAR, END_YEAR):
-    # change_times
-    #  - summer
-    #  - winter
-    change_times = []  
-    for c in range(0, NB_CHANGE):
-        res = []
-        for i in tzs:
-            ct = get_tz_transition_time(i, y)
-            res.append(ct[c])
-        change_times.append(res)
-    for c in  range(0, NB_CHANGE):
-        begin = change_times[c][0]
-        for d in change_times[c]:
-            if begin > d:
-                begin = d
+def get_tz_transition_time(tz, year):
+    return [x for x in tz._utc_transition_times if x.year == year]
 
-        end = change_times[c][0]
-        for d in change_times[c]:
-            if end < d:
-                end = d
+def get_calendar(tzs, start_year, end_year):
+    cal = Calendar()
+    for y in range(start_year, end_year):
+        # change_times
+        #  - summer
+        #  - winter
+        change_times = []  
+        for c in range(0, NB_CHANGE):
+            res = []
+            for i in tzs:
+                ct = get_tz_transition_time(i, y)
+                res.append(ct[c])
+            change_times.append(res)
+        for c in  range(0, NB_CHANGE):
+            begin = change_times[c][0]
+            for d in change_times[c]:
+                if begin > d:
+                    begin = d
 
-
-        uid = hashlib.md5((begin.isoformat()).encode()).hexdigest()
-        if c == 0:
-            p = "Summer time"
-        else:
-            p = "Winter time"
-        ev = Event(name = "Timezone trouble period - %s %s" % (p, y),
-                   uid = uid,
-                   begin = begin,
-                   end = end)
-        ev.make_all_day()
-        cal.events.add(ev)
+            end = change_times[c][0]
+            for d in change_times[c]:
+                if end < d:
+                    end = d
 
 
-with open(OUTPUT, 'w') as f:
-    f.write(str(cal))
-    
+            uid = hashlib.md5((begin.isoformat()).encode()).hexdigest()
+            if c == 0:
+                p = "Summer time"
+            else:
+                p = "Winter time"
+            ev = Event(name = "Timezone trouble period - %s %s" % (p, y),
+                       uid = uid,
+                       begin = begin,
+                       end = end)
+            ev.make_all_day()
+            cal.events.add(ev)
+    return cal
+
+if __name__ == '__main__':
+    tzs = get_tzs_args()
+    cal = get_calendar(tzs, START_YEAR, END_YEAR)
+    with open(OUTPUT, 'w') as f:
+        f.write(str(cal))
+
